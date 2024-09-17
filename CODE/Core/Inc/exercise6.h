7598f398333181033d5a17e04b24778b31841600
vohoanghuy <vohoanghuy7705@gmail.com>
2024-09-17 19:55:17 +0700

    init for Ex6

---
+++ CODE/Core/Inc/exercise6.h
@@ -0,0 +1,16 @@
+/*
+ * exercise6.h
+ *
+ *  Created on: Sep 17, 2024
+ *      Author: VO HOANG HUY
+ */
+
+#ifndef INC_EXERCISE6_H_
+#define INC_EXERCISE6_H_
+
+#include "main.h"
+
+void init_exercise6();
+void exercise6_run();
+
+#endif /* INC_EXERCISE6_H_ */

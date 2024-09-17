5536e6eb83ac1adf9d7903afb2a93bfab6e7c570
vohoanghuy <vohoanghuy7705@gmail.com>
2024-09-17 20:17:23 +0700

    init for Ex7

---
+++ CODE/Core/Inc/exercise7.h
@@ -0,0 +1,16 @@
+/*
+ * exercise7.h
+ *
+ *  Created on: Sep 17, 2024
+ *      Author: VO HOANG HUY
+ */
+
+#ifndef INC_EXERCISE7_H_
+#define INC_EXERCISE7_H_
+
+#include "main.h"
+void init_exercise7();
+void exercise7_run();
+void clearAllClock();
+
+#endif /* INC_EXERCISE7_H_ */

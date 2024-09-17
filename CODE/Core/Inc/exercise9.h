6bec1718e29f2350ce5b288b05991a6ff54dba9f
vohoanghuy <vohoanghuy7705@gmail.com>
2024-09-17 20:55:06 +0700

    init for Ex9

--- CODE/Core/Inc/exercise9.h
+++ CODE/Core/Inc/exercise9.h
@@ -4,10 +4,12 @@
  *  Created on: Sep 17, 2024
  *      Author: VO HOANG HUY
  */
-
+#include "main.h"
 #ifndef INC_EXERCISE9_H_
 #define INC_EXERCISE9_H_
 
-
+void init_exercise9();
+void exercise9_run();
+void clearNumberOnClock(int num);
 
 #endif /* INC_EXERCISE9_H_ */

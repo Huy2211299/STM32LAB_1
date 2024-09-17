e7a6a890fd7124bde3d08fe9f1f63011a92a36d1
vohoanghuy <vohoanghuy7705@gmail.com>
2024-09-17 20:29:21 +0700

    init for Ex8

---
+++ CODE/Core/Inc/exercise8.h
@@ -0,0 +1,16 @@
+/*
+ * exercise8.h
+ *
+ *  Created on: Sep 17, 2024
+ *      Author: VO HOANG HUY
+ */
+#include "main.h"
+#ifndef INC_EXERCISE8_H_
+#define INC_EXERCISE8_H_
+
+
+void init_exercise8();
+void exercise8_run();
+void setNumberOnClock(int num);
+
+#endif /* INC_EXERCISE8_H_ */

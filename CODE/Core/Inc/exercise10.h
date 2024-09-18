ba22b20bb82785f16926ec29e7e1658a106e36bf
vohoanghuy <vohoanghuy7705@gmail.com>
2024-09-18 11:43:24 +0700

    init for Ex10

---
+++ CODE/Core/Inc/exercise10.h
@@ -0,0 +1,16 @@
+/*
+ * exercise10.h
+ *
+ *  Created on: Sep 18, 2024
+ *      Author: VO HOANG HUY
+ */
+#include "main.h"
+
+#ifndef INC_EXERCISE10_H_
+#define INC_EXERCISE10_H_
+
+void init_exercise10();
+void exercise10_run();
+
+
+#endif /* INC_EXERCISE10_H_ */
--- CODE/Core/Src/main.c
+++ CODE/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "exercise10.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,15 +84,18 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  init_exercise10();
   while (1)
   {
-	  //TO DO Ex9
+	  exercise10_run();
+	  HAL_Delay(10);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -132,6 +136,44 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, led0_Pin|led1_Pin|led2_Pin|led3_Pin
+                          |led4_Pin|led5_Pin|led6_Pin|led7_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, led8_Pin|led9_Pin|led10_Pin|led11_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : led0_Pin led1_Pin led2_Pin led3_Pin
+                           led4_Pin led5_Pin led6_Pin led7_Pin */
+  GPIO_InitStruct.Pin = led0_Pin|led1_Pin|led2_Pin|led3_Pin
+                          |led4_Pin|led5_Pin|led6_Pin|led7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : led8_Pin led9_Pin led10_Pin led11_Pin */
+  GPIO_InitStruct.Pin = led8_Pin|led9_Pin|led10_Pin|led11_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
